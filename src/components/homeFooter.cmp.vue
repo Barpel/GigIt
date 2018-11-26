e1aac837fbbdfc558785f59751aa5e8bd2fc79e5
Barpel <42574095+Barpel@users.noreply.github.com>
2018-11-26 20:28:17 +0200

    seperated to scss files

--- src/components/homeFooter.cmp.vue
+++ src/components/homeFooter.cmp.vue
@@ -1,7 +1,21 @@
 <template>
-    <footer>
-        <h1>Gig<span>It</span></h1>
-    </footer>
+  <footer>
+    <h1>
+      Gig
+      <span>It</span>
+    </h1>
+    <div class="social-links-container">
+      <a href="#">
+        <i class="fab fa-facebook"></i>
+      </a>
+      <a href="#">
+        <i class="fab fa-instagram"></i>
+      </a>
+      <a href="#">
+        <i class="fab fa-linkedin"></i>
+      </a>
+    </div>
+  </footer>
 </template>
 
 <script>
@@ -12,8 +26,8 @@ name: 'homeFooter',
 </script>
 <style lang="scss" scoped>
 footer {
-    min-height: 40vh;
-    background-color: #333333d0;
+    min-height: 35vh;
+    background-color: #424242d0;
     h1 {
         font-family: condiment;
             color: white;
@@ -21,8 +35,23 @@ footer {
             padding: 10px;
             letter-spacing: 1px;
             font-size: 2.5rem;
+            margin: 0;
         span {
             color: #97a5a7;
+        }
+    }
+    .social-links-container{
+        display: grid;
+        grid-gap: 25px;
+        grid-template-columns: repeat(3, 1fr);
+        grid-template-rows: 1fr;
+        max-width: fit-content;
+        margin: 0 auto;
+        a {
+            color: white;
+            i{
+                font-size: 4rem;
+            }
         }
     }
 }
