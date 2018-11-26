38fe6558c94a0d7ccba24a0dbf959db7cdba9f99
Barpel <42574095+Barpel@users.noreply.github.com>
2018-11-26 22:05:45 +0200

    ending day 1

--- src/components/homeFooter.cmp.vue
+++ src/components/homeFooter.cmp.vue
@@ -1,8 +1,7 @@
 <template>
-  <footer>
+  <footer class="home-footer">
     <h1>
-      Gig
-      <span>It</span>
+      Gig<span>It</span>
     </h1>
     <div class="social-links-container">
       <a href="#">
@@ -25,36 +24,5 @@ name: 'homeFooter',
 }
 </script>
 <style lang="scss" scoped>
-footer {
-    min-height: 35vh;
-    background-color: #424242d0;
-    h1 {
-        font-family: condiment;
-            color: white;
-            text-align: left;
-            padding: 10px;
-            letter-spacing: 1px;
-            font-size: 2.5rem;
-            margin: 0;
-        span {
-            color: #97a5a7;
-        }
-    }
-    .social-links-container{
-        display: grid;
-        grid-gap: 25px;
-        grid-template-columns: repeat(3, 1fr);
-        grid-template-rows: 1fr;
-        max-width: fit-content;
-        margin: 0 auto;
-        a {
-            color: white;
-            i{
-                font-size: 4rem;
-            }
-        }
-    }
-}
-
 </style>
 
