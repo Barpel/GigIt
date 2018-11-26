d82d0d3ce060febcd23b530fb79e55f95f5b525c
Gabi Chami <thegabibi@gmail.com>
2018-11-26 16:27:49 +0200

    First commit

---
+++ service/localGlobalService.js
@@ -0,0 +1,14 @@
+var items = [{
+    id: 1
+}, { id: 2 }]
+
+
+
+function query() {
+    if (items) return items
+}
+
+function getById(itemId = 1) {
+    let currItem = items.find(item => item.id === itemId)
+    console.log(currItem)
+}
