2f1300449aea5454920b8d4ae4df4c36b28d9399
Barpel <42574095+Barpel@users.noreply.github.com>
2018-11-26 18:39:26 +0200

    categories ready, footer in progress. waiting for list to add to home page

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,32 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link>
-      <router-link to="/about">About</router-link>
-      <router-link to="/gigs">Gigs</router-link>
-      <router-link to="/login">Login</router-link>
+      <router-link to="/">
+        <h1>
+          <i class="fas fa-home"></i>
+          Home
+        </h1>
+      </router-link>
+      <router-link to="/about">
+        <h1>
+          <span>
+            Gig<span>It</span>
+          </span>
+          About
+        </h1>
+      </router-link>
+      <router-link to="/gigs">
+        <h1>
+          <i class="fas fa-th-large"></i>
+          Gigs
+        </h1>
+      </router-link>
+      <router-link to="/login">
+        <h1>
+          <i class="fas fa-user"></i>
+          Login
+        </h1>
+      </router-link>
     </div>
     <router-view/>
   </div>
@@ -21,20 +43,36 @@
   min-height: 100vh;
 }
 #nav {
+  max-height: 100px;
   position: fixed;
   bottom: 0;
   width: 100%;
-  padding: 30px;
+  padding: 20px;
   background-color: #343434;
   color: white;
   border: 1px solid black;
   display: grid;
   grid-template-columns: repeat(4, 1fr);
+  margin: 20px 0 0;
+  
   z-index: 10;
   a {
     text-decoration: none;
     font-weight: bold;
     color: white;
+    h1 {
+      margin: 0;
+      height: 100%;
+      display: grid;
+      grid-template-rows: 1fr 1fr;
+      font-size: 1.5rem;
+      span {
+        font-family: condiment;
+        span {
+          color: #97a5a7;
+        }
+      }
+    }
     &.router-link-exact-active {
       color: #2ca2af;
     }
---
+++ src/components/homeFooter.cmp.vue
@@ -0,0 +1,31 @@
+<template>
+    <footer>
+        <h1>Gig<span>It</span></h1>
+    </footer>
+</template>
+
+<script>
+export default {
+name: 'homeFooter',
+
+}
+</script>
+<style lang="scss" scoped>
+footer {
+    min-height: 40vh;
+    background-color: #333333d0;
+    h1 {
+        font-family: condiment;
+            color: white;
+            text-align: left;
+            padding: 10px;
+            letter-spacing: 1px;
+            font-size: 2.5rem;
+        span {
+            color: #97a5a7;
+        }
+    }
+}
+
+</style>
+
