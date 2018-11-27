914985736ae8ace7efcbc19b1eb402f9356dc583
Barpel <42574095+Barpel@users.noreply.github.com>
2018-11-27 15:20:27 +0200

    updating

--- src/views/userGigs.vue
+++ src/views/userGigs.vue
@@ -1,7 +1,5 @@
 <template>
-  <section v-if="user">
-      {{user.gigsIds}}
-  </section>
+  <section v-if="user">{{user.gigsIds}}</section>
 </template>
 
 <script>
@@ -15,7 +13,17 @@ export default {
         console.log('user gigs')
         var userId = this.$route.params.userId
         this.$store.dispatch({type: 'getUserById', userId})
-}
+        .then(() => {
+            this.getUserGigs()
+        })
+    },
+    methods: {
+        getUserGigs() {
+            var userGigIds = this.user.gigsIds;
+            console.log('userGigIds', userGigIds)
+        }
+    }
+
 }
 </script>
 
