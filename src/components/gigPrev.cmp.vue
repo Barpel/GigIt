eab434b1dd8c2934f9b77fa7de790136ec261cc0
Barpel <42574095+Barpel@users.noreply.github.com>
2018-11-28 16:50:39 +0200

    preview fixed to look better

--- src/components/gigPrev.cmp.vue
+++ src/components/gigPrev.cmp.vue
@@ -2,8 +2,32 @@
   <li>
     <div :class="classByCategory" class="gig-image"></div>
     <div>
-      <h3>{{gig.details.title}}</h3>
-      <p>{{gig.details.pos.dist}}</p>
+      <div class="publisher-container">
+        <img src="http://www.dalton-winery.com/wp-content/uploads/2017/03/GUY.jpg">
+        <div class="publisher-info">
+          <h3>Jacob Ben-Lulu</h3>
+          <h3>
+            <i class="fas fa-star"></i>
+            &nbsp;
+            <span>4.2</span>
+          </h3>
+        </div>
+      </div>
+      <!-- <div class="publisher-container" v-if="publisher">
+        <img :src="publisher.img">
+        <h3>{{publisher.name.first}} {{publisher.name.last}}</h3>
+        <h3>⭐{{publisher.reviews.totalAverage}}</h3>
+      </div>-->
+      <div class="gig-details">
+        <h3>{{gig.details.title}}</h3>
+        <p>{{gig.details.pos.dist}}</p>
+      </div>
+      <div class="lower-bar-container">
+        <p>{{gig.details.price}}</p>
+        <button>
+          Gig<span>It</span>
+        </button>
+      </div>
     </div>
   </li>
 </template>
@@ -11,7 +35,8 @@
 <script>
 export default {
     props: {
-        gig:Object
+        gig: Object,
+        publisher: Object,
     },
     computed: {
         classByCategory() {
@@ -22,10 +47,11 @@ export default {
                 'house-work': this.gig.category==='house-work',
                 'line-queue': this.gig.category==='line-queue',
                 'pet-care': this.gig.category==='pet-care',
-                
-
             }
         }
+    },
+    mounted() {
+        console.log(this.gig)
     }
 
 }
