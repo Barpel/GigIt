0da2965853778eab37a1bc89ff4caa9ac2cd59d0
Yehonatan Barr <jonybh99@gmail.com>
2018-11-29 19:57:28 +0200

    server connected

--- src/service/gigService.js
+++ src/service/gigService.js
@@ -3,7 +3,7 @@
 import axios from 'axios';
 
 // const BASE_URL = 'http://localhost:3000';
-const BASE_URL = (process.env.NODE_ENV !== 'development')? '/gig': '//localhost:3000';
+const BASE_URL = (process.env.NODE_ENV !== 'development')? '/gig': '//localhost:3000/gig';
 
 
 export default {
@@ -14,14 +14,17 @@ export default {
 }
 
 function query(filter = {}) {
-    var queryParams = new URLSearchParams();
-    for (let key in filter) {
-        if (key === 'sort') continue;
-        queryParams.set(`${key}`, filter[key]);
-    }
-    // console.log('query params', queryParams.toString())
-    return axios.get(`${BASE_URL}?${queryParams}`)
-        .then(res => res.data);
+    return axios.get(BASE_URL)
+        .then(res => res.data)
+    // console.log('im herr!')
+    // var queryParams = new URLSearchParams();
+    // for (let key in filter) {
+    //     if (key === 'sort') continue;
+    //     queryParams.set(`${key}`, filter[key]);
+    // }
+    // // console.log('query params', queryParams.toString())
+    // return axios.get(`${BASE_URL}?${queryParams}`)
+    //     .then(res => res.data);
 }
 function getById(gigId) {
     return axios.get(`${BASE_URL}/${gigId}`)
