3a420aa5295c50e82a4841d44e24a9b318e2415f
Barpel <42574095+Barpel@users.noreply.github.com>
2018-11-29 17:33:05 +0200

    added element io and gig edit in progress

--- src/main.js
+++ src/main.js
@@ -7,6 +7,12 @@ import './registerServiceWorker'
 
 import './scss/main.scss'
 
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import locale from 'element-ui/lib/locale/lang/en'
+Vue.use(ElementUI, { locale });
+
+
 Vue.config.productionTip = false
 
 
