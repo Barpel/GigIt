5e29bc0b8c6d5c9fb50b547b65b5701c70011d9a
Barpel <42574095+Barpel@users.noreply.github.com>
2018-12-03 19:16:39 +0200

    new home design

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,5 +1,28 @@
 <template>
   <div class="home">
+    <div class="banner">
+      <div class="welcome-container">
+        <img :src="user.img" v-if="user">
+        <i class="fas fa-user-circle" v-else></i>
+        <h1>Hi {{(user)? user.name.first: 'Guest'}}</h1>
+        <router-link to="/gig/edit" tag="button" v-if="user">
+          <i class="fas fa-plus"></i>
+          <span>Gig</span>
+        </router-link>
+        <router-link to="/user/login" tag="button" v-else>Login</router-link>
+      </div>
+
+      <div class="block">
+        <el-carousel trigger="click" :interval="7000">
+          <el-carousel-item v-for="(carouselItem, idx) in carouselObjs" :key="idx">
+            <h3>{{carouselItem.txt}}</h3>
+            <img :src="carouselItem.imgUrl">
+            <h4>Gig<span>It</span></h4>
+          </el-carousel-item>
+        </el-carousel>
+      </div>
+    </div>
+    <hr class="hr1">
     <div class="cover-container">
       <h1>
         Gig
@@ -19,6 +42,8 @@
         <button>Search</button>
       </div>
     </div>
+
+    
     <gig-list :gigs="gigs" @gigClicked="gigClicked"/>
     <hr v-if="showCategories">
     <gig-categories v-if="showCategories"/>
@@ -27,7 +52,6 @@
 </template>
 
 <script>
-// @ is an alias to /src
 import gigCategories from "@/components/gigCategories.cmp.vue";
 import homeFooter from "@/components/homeFooter.cmp.vue";
 import gigList from "@/components/gigList.vue";
@@ -36,11 +60,34 @@ export default {
   data() {
     return {
       coverCounter: 0,
-      filter:{
-          byCategory: '',
-          byTitle:''
+      filter: {
+        byCategory: "",
+        byTitle: ""
+      },
+      showCategories: true,
+      user: null,
+      carouselObjs: [
+        {
+          imgUrl:
+            "https://res.cloudinary.com/barpel/image/upload/v1543855824/GigIt/mechanic.png",
+          txt: "Use your skills to make some easy extra money!"
         },
-      showCategories: true
+        {
+          imgUrl:
+            "https://res.cloudinary.com/barpel/image/upload/v1543855819/GigIt/giger.png",
+          txt: "Helping the community is part of our agenda. Help out today!"
+        },
+        {
+          imgUrl:
+            "https://res.cloudinary.com/barpel/image/upload/v1543855817/GigIt/cleaning-boy.png",
+          txt: "Sometimes cleaning is too tiring. Get a Gigster to help you out!"
+        },
+        {
+          imgUrl:
+            "https://res.cloudinary.com/barpel/image/upload/v1543855818/GigIt/garden-lady.png",
+          txt: "People all around want to help you take care of your garden!"
+        }
+      ]
     };
   },
   computed: {
@@ -57,9 +104,9 @@ export default {
       this.$router.push(`gig/${gigId}`);
     },
     filterByKey() {
-      this.showCategories = false
-      this.$store.dispatch({ type: "filterByKey", filter: this.filter })
-        // .then()
+      this.showCategories = false;
+      this.$store.dispatch({ type: "filterByKey", filter: this.filter });
+      // .then()
     }
   },
   components: {
@@ -77,8 +124,8 @@ export default {
     //   }, 5000);
   },
   watch: {
-    'filter.byTitle'() {
-      if (!this.filter.byTitle) this.showCategories = true
+    "filter.byTitle"() {
+      if (!this.filter.byTitle) this.showCategories = true;
     }
   }
 };
