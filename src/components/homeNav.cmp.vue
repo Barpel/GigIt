6b23ed3ada75f815b4e21f9175bfac6c461f2a9a
Barpel <42574095+Barpel@users.noreply.github.com>
2018-12-03 13:15:48 +0200

    updated home screen and navbar

--- src/components/homeNav.cmp.vue
+++ src/components/homeNav.cmp.vue
@@ -1,19 +1,10 @@
 <template>
   <section>
-    <div id="home-nav">
+    <div class="home-nav">
       <router-link to="/">
         <h1>
           <i class="fas fa-home"></i>
           Home
-        </h1>
-      </router-link>
-      <router-link to="/about">
-        <h1>
-          <span>
-            Gig
-            <span>It</span>
-          </span>
-          About
         </h1>
       </router-link>
       <router-link to="/gig">
@@ -22,43 +13,25 @@
           Gigs
         </h1>
       </router-link>
-      <a @click="onUserClicked" class="home-nav-user">
-        <h1 class="profile-info-container">
-          <i class="fas fa-user">
-            <div class="notification-circle" v-if="isLoggedin"></div>
-          </i>
-          {{isLogin}}
-        </h1>
-      </a>
-    </div>
-    <div class="user-dropdown" :class="dropdownClass">
-      <router-link @click.native="closeDropdown" :to="myGigsLink">
-        <h1>
-          <span>Gigs</span>
-          My Gigs
-        </h1>
-      </router-link>
-      <router-link @click.native="closeDropdown" :to="completedGigsLink">
-        <h1>
-          <i class="fas fa-check-square"></i>
-          Completed
-        </h1>
-      </router-link>
-      <router-link @click.native="closeDropdown" :to="inboxLink">
+      <router-link :to="(isLoggedin)?inboxLink: '/user/login'">
         <h1>
           <i class="fas fa-envelope"></i>
           Inbox
         </h1>
       </router-link>
-      <router-link @click.native="closeDropdown" :to="profileLink">
+      <router-link :to="(isLoggedin)?myGigsLink: '/user/login'">
         <h1>
-          <i class="fas fa-id-card"></i>
-          My Profile
+          <span class="logo-i">Gigs</span>
+          My Gigs
         </h1>
       </router-link>
-      <div class="dropdown-btn-cls" @click="closeDropdown">
-        <i class="fas fa-times"></i>
-      </div>
+
+      <router-link :to="(isLoggedin)?profileLink : '/user/login' ">
+        <h1>
+          <i class="fas fa-user"></i>
+          {{(isLoggedin)? 'Profile': 'Login'}}
+        </h1>
+      </router-link>
     </div>
   </section>
 </template>
@@ -73,16 +46,11 @@ export default {
   },
   computed: {
     user() {
-      if(this.$store.getters.user) return this.$store.getters.user
-      else return {}
+      if (this.$store.getters.user) return this.$store.getters.user;
+      else return {};
     },
     isLoggedin() {
       return this.$store.getters.isLoggedin;
-    },
-    isLogin() {
-      var isLogged = this.isLoggedin;
-      if (isLogged) return "Profile";
-      else return "Login";
     },
     profileLink() {
       return `/user/${this.user._id}`;
@@ -90,26 +58,12 @@ export default {
     myGigsLink() {
       return `/user/${this.user._id}/gigs`;
     },
-    completedGigsLink() {
-      return `/user/${this.user._id}/completed`;
-    },
     inboxLink() {
       return `/user/${this.user._id}/inbox`;
     },
-    dropdownClass() {
-        if(this.isUserDropdown) return 'open'
-    }
+    
   },
-  methods: {
-    closeDropdown() {
-      this.isUserDropdown = false;
-    },
-    onUserClicked() {
-      if (this.isLoggedin) {
-        this.isUserDropdown = !this.isUserDropdown;
-      } else this.$router.push('/user/login');
-    }
-  }
+
 };
 </script>
 
