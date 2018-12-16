4dc5a0f34db16051680483ba23566bb48fec7d6a
Barpel <42574095+Barpel@users.noreply.github.com>
2018-12-16 13:56:59 +0200

    completed gig is ready...or what we have of it

--- src/components/utils/completedGigs.cmp.vue
+++ src/components/utils/completedGigs.cmp.vue
@@ -1,25 +1,36 @@
 <template>
-    <section class="completed-gigs-container">
-        <li v-for="gig in gigs" :key="gig._id">
-            <img :src="gig.pendingUsers[0].img" />
-            {{gig.pendingUsers[0].name}}
-            {{gig.details.title}}
-            {{gig.details.price}}
-            <!-- {{gig.pendingUsers[0].}} -->
-        </li>
-    </section>
+  <section class="completed-gigs-container">
+    <li v-for="gig in gigs" :key="gig._id">
+      <div class="completed-gigster" 
+      @click="goToGigsterProfile(gig.pendingUsers[0].id)">
+        <img :src="gig.pendingUsers[0].img" title="Go to Gigster's profile">
+        <h3>{{gig.pendingUsers[0].name}}</h3>
+      </div>
+      <h2 @click="goToGig(gig._id)" title="Title">{{gig.details.title}}</h2>
+      <h3>{{gig.details.price}}₪</h3>
+      <span>
+        <i class="fas fa-check-square"></i>
+      </span>
+    </li>
+  </section>
 </template>
 
 <script>
 export default {
-    props: ['gigs'],
-    created() {
-        console.log(this.gigs)
+  props: ["gigs"],
+  methods: {
+    goToGigsterProfile(gigsterId) {
+      this.$router.push(`/user/${gigsterId}`);
+    },
+    goToGig(gigId) {
+        this.$router.push(`/gig/${gigId}`)
     }
-
-}
+  },
+  created() {
+    console.log(this.gigs);
+  }
+};
 </script>
 
 <style>
-
 </style>
